7a8167314741dacfe93d07773cb542c86c316661
Daniel Pinzón <justdanielfloyd@gmail.comgit config --global user.name Daniel@>
2022-10-21 18:20:21 +0200

    typescriptfixessssss

--- pages/contact.js
+++ pages/contact.js
@@ -1,107 +1,95 @@
-import NextLink from 'next/link'
-import { useState } from 'react'
+// components/ContactForm.js
+
+import React, { useState } from "react";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+import Layout from "../components/layouts/article";
 import {
     Container, Box, Heading, Button,
     VStack, Wrap, WrapItem, FormControl,
     FormLabel, Input, InputGroup, InputLeftElement, Textarea,
 } from '@chakra-ui/react';
-import { MdOutlineEmail, } from 'react-icons/md';
 import { BsPerson } from 'react-icons/bs';
-import Layout from '../components/layouts/article';
-import { ChevronRightIcon } from '@chakra-ui/icons'
+import { ChevronRightIcon } from '@chakra-ui/icons';
+import { MdOutlineEmail, } from 'react-icons/md';
 
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm();
+  const [successMessage, setSuccessMessage] = useState("");
 
-const Contact = () => {
-    const [fullname, setFullname] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-    
-    const [submitted, setSubmitted] = useState(false);
-    const handleSubmit = () => {
-        setTimeout(() => {
-          setSubmitted(true);
-        }, 100);
-      };
-
-      if (submitted) {
-        return (
-          <>
-            <div className="text-2xl">Thank you!</div>
-            <div className="text-md">Well be in touch soon.</div>
-          </>
+  function onSubmit(data) {
+    axios
+      .post("https://eoa2ze0905pvmxl.m.pipedream.net", data)
+      .then((response) => {
+        setSuccessMessage(
+          `¡Gracias por tu mensaje! 😊 Pronto me pondré en contacto contigo.`
         );
-      }
+      })
+      .catch((e) => console.error(e));
+  }
 
-    const ContactForm = (
-
-        <Layout>
-            <Container>
-                <Heading as="h3" align="center" mt={8}>
-                    Contacto
-                </Heading>
-                <Box color="white" borderRadius="lg" align="center" my={4} p={16}>
-                    <Box p={4}>
-                        <Wrap>
-                            <WrapItem>
-                                <Box bg="white" borderRadius="lg">
-                                    <Box m={8} color="#0B0E3F">
-                                        <VStack spacing={5}>                             
-                                            <form onSubmit={handleSubmit} method="POST" name="contact-form" action="contact/?success=true" data-netlify="true" data-netlify-honeypot="bot-field">
-                                            <FormControl id="name">
-                                                <FormLabel>Tu nombre</FormLabel>
-                                                <InputGroup borderColor="#E0E1E7">
-                                                    <InputLeftElement pointerEvents="none">
-                                                        <BsPerson color="gray.800" />
-                                                    </InputLeftElement>
-                                                    <Input name="name" required onChange={(e) => setFullname(e.target.value)} type="text" size="md" value={fullname}/>
-                                                </InputGroup>
-                                            </FormControl>
-                                            <FormControl id="email">
-                                                <FormLabel>Correo electrónico</FormLabel>
-                                                <InputGroup borderColor="#E0E1E7">
-                                                    <InputLeftElement pointerEvents="none">
-                                                        <MdOutlineEmail color="gray.800" />
-                                                    </InputLeftElement>
-                                                
-                                                    <Input name="email" required onChange={(e) => setEmail(e.target.value)} type="text" size="md" value={email} />
-                                                </InputGroup>
-                                            </FormControl>
-                                            <FormControl id="name">
-                                                <FormLabel>Mensaje</FormLabel>
-                                                <Textarea value={message} required onChange={(e) => {setMessage(e.target.value);}}
+  return (
+    <Layout>
+        <Container>
+            <Heading as="h3" alignContent="center" mt="8">
+                Contacto
+            </Heading>
+            <Box color="white" borderRadius="lg" align="center" my={4} p={16}>
+                <Box p={4}>
+                    <Wrap>
+                        <WrapItem>
+                            <Box bg="white" borderRadius="lg">
+                                <Box m={8} color="#0B0E3F">
+                                <VStack spacing={5}>
+                                    <form onSubmit={handleSubmit(onSubmit)}>
+                                        <FormControl id="name">
+                                            <FormLabel>Tu nombre</FormLabel>
+                                            <InputGroup borderColor="#E0E1E7">
+                                                <InputLeftElement pointerEvents="none">
+                                                    <BsPerson color = "gray.800"/>
+                                                </InputLeftElement>
+                                                <Input {...register("name")} name="name" required type="text" size="md" />
+                                            </InputGroup>
+                                        </FormControl>
+                                        <FormControl id="email">
+                                            <FormLabel>Correo electrónico</FormLabel>
+                                            <InputGroup borderColor="#E0E1E7">
+                                                <InputLeftElement pointerEvents="none">
+                                                    <MdOutlineEmail color="gray.800" />
+                                                </InputLeftElement>
+                                                <Input {...register("email")} name="email" required type="text" size="md" />
+                                            </InputGroup>
+                                        </FormControl>
+                                        <FormLabel>Mensaje</FormLabel>
+                                                <Textarea required
                                                     borderColor="gray.300"
                                                     _hover={{
                                                         borderRadius: 'gray.300',
                                                     }}
                                                     placeholder="Escribe tu mensaje"
                                                 />
-                                            </FormControl>
-                                            <FormControl id="name" float="right">
-                                                <Box align="center" my={4}>
-                                                    <NextLink href="/#" passHref scroll={false}>
-                                                        <Button type="submit" rightIcon={<ChevronRightIcon />} colorScheme="teal">
-                                                            Enviar mensaje
-                                                        </Button>
-                                                    </NextLink>
-                                                </Box>
-                                            </FormControl>
-                                            </form>
-                                        </VStack>
-                                    </Box>
+                                        <FormControl id="mensaje">
+                                            
+                                        </FormControl>
+
+                                    <button role="submit" rightIcon={<ChevronRightIcon />}>{isSubmitting ? "Enviando mensaje..." : "Enviar"}</button>
+                                    {successMessage && <p>{successMessage}</p>}
+                                    </form>
+                                </VStack>
                                 </Box>
-                            </WrapItem>
-                        </Wrap>
-                    </Box>
+                            </Box>
+                        </WrapItem>
+                    </Wrap>
                 </Box>
-            </Container>
-        </Layout>
-    );
+            </Box>
+        </Container>
+    </Layout>
 
-    return (
-        <div className="container">
-            <main></main>
-        </div>
-    );
-};
+  );
+}
 
-export default ContactForm;
+
